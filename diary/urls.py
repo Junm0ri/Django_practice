519bf01100a6c10ff6fb6c1320676c225da67cbb
Jun Moriuchi <79455149+Junm0ri@users.noreply.github.com>
2022-09-27 20:45:56 +0900

    トップページのデザインを作成

--- diary/urls.py
+++ diary/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .import views
 
-app_ame='diary'
+app_name='diary'
 urlpatterns=[
     path('',views.IndexView.as_view(),name="index"),
 ]
--- private_diary/settings.py
+++ private_diary/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%lon6fm+8+xh!ik_dmf12oa&m_cqysm^hq%k-wd1-juel4wjwt'
+SECRET_KEY = 'django-insecure-6o$_dt(x!j+k6*av0_v7)z8h3i0aa!7s9eey0%8gxe72jz7gb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'diary.apps.DiaryConfig',
 ]
 
@@ -64,6 +64,7 @@ WSGI_APPLICATION = 'private_diary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# データベース設定
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -113,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
